88fa7ba3d36d24ad60a99a4157d9cea26d0cc088
Patrick Buller <pbuller212@gmail.com>
2025-03-12 07:18:11 -0500

    Basic chatgpt submission seems to work. Just need to parse the response better.

--- main.py
+++ main.py
@@ -24,6 +24,35 @@ default_prompt = "Please transcribe these handwritten notes into text?"
 # app = Django(EXTRA_APPS=['django_q',])
 app = Django()
 
+def encode_image(record):
+    with open(record.image.path, "rb") as image_file:
+        image = base64.b64encode(image_file.read()).decode("utf-8")
+    return image
+
+def openai_request(record, prompt):
+    image = encode_image(record)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt,
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{image}"},
+                    },
+                ],
+            }
+        ],
+    )
+    t = Translation.objects.create(record=record, prompt=prompt, results=response.choices[0])
+    t.save()
+    return t
+
 @register.filter
 def get_item(dictionary, key):
     if dictionary is None or isinstance(dictionary, str) or key is None:
@@ -71,6 +100,12 @@ def index(request):
 def record(request, record_id=None):
     if record_id is not None:
         record = get_object_or_404(Record, pk=record_id)
+        translations = record.translations.all()
+    else:
+        return redirect("index")
+    if request.method == "POST":
+        prompt = request.POST.get('prompt', '')
+        new_translation = openai_request(record, prompt)
         translations = record.translations.all()
     context = {
         "record": record,
